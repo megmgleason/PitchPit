ead572f7250d8bfbbce1186671222d114240ff0f
Apple <apple@Apples-MacBook-Pro-98.local>
2019-08-15 12:12:58 -0700

    buttons display messages

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-var correctButton1= document.querySelector("#firstRoundBFlat");
+var correctButton1= document.getElementById("#firstRoundBFlat");
+var showMessage=document.getElementById("#emptyFirstRound");
+var paragraph=document.getElementById("#emptyFirstRound");
 
-if (correctButton1=="click"){
-  console.log("you are correct!")
-} else{
-  console.log("that wasn't right, try again")
+// correctButton1.addEventListener("click", showMesage1);
+
+if(correctButton1){
+  correctButton1.addEventListener('click', showMesage1);
 }
+
+
+// function showMesage1(){
+  // if (correctButton1=="correctButton1"){
+  //   paragraph.innerHTML="you are correct"
+  //   console.log("butt");
+  //
+  // } else{
+  //   paragraph.innerHTML="that wasn't it, try again"
+  //   console.log("code");
+  // }
+
+//   if (correctButton1.onclick == true){
+//     alert("nice");
+//   }
+//   else{
+//     alert("not nice");
+//   }
+//
+//
+//
+// }
+// correctButton1.onclick = function() {
+//  alert("button was clicked");
+// }​;​
