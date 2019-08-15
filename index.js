b91602ff8d354417bf48621c6043f0df9957d7d4
Apple <apple@Apples-MacBook-Pro-98.local>
2019-08-15 08:58:21 -0700

    sounds and buttons linked

---
+++ index.js
@@ -0,0 +1,7 @@
+var correctButton1= document.querySelector("#firstRoundBFlat");
+
+if (correctButton1=="click"){
+  console.log("you are correct!")
+} else{
+  console.log("that wasn't right, try again")
+}
